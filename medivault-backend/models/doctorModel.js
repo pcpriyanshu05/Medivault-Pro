9be7c23d9f1476c9c21ea98b6de36ad05486629f
Priyanshu <pcauthentic123@gmail.com>
2025-07-19 17:43:43 +0530

    Frontend again added

--- medivault-backend/models/doctorModel.js
+++ medivault-backend/models/doctorModel.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema({
-_id: String, // e.g. DOC001
-name: String,
-specialization: String,
-experience_years: Number,
-hospital_id: String, // linked to hospital
-contact_email: String,
-phone: String
+  _id: String, // e.g. DOC001
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true
+  },
+  name: String,
+  specialization: String,
+  experience_years: Number,
+  hospital_id: String, // linked to hospital
+  contact_email: String,
+  phone: String
 });
 
 module.exports = mongoose.model("Doctor", doctorSchema);
